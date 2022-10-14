69556f6585c6b90964878e983f690d0ad512b19f
Joakim Skoglund <joakim@voltura.se>
2022-10-15 00:07:29 +0200

    Add project files.

---
+++ EarnerForm.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Earner
+{
+    partial class EarnerForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this._lblEarned = new System.Windows.Forms.Label();
+            this._btnOptions = new System.Windows.Forms.Button();
+            this._btnStart = new System.Windows.Forms.Button();
+            this._earnerTimer = new System.Windows.Forms.Timer(this.components);
+            this._lblWorkTime = new System.Windows.Forms.Label();
+            this._btnHide = new System.Windows.Forms.Button();
+            this._topPanel = new System.Windows.Forms.Panel();
+            this._btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // _lblEarned
+            // 
+            this._lblEarned.AutoSize = true;
+            this._lblEarned.BackColor = System.Drawing.Color.Transparent;
+            this._lblEarned.Font = new System.Drawing.Font("Segoe UI Light", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this._lblEarned.ForeColor = System.Drawing.Color.White;
+            this._lblEarned.Location = new System.Drawing.Point(-10, 26);
+            this._lblEarned.Name = "_lblEarned";
+            this._lblEarned.Size = new System.Drawing.Size(495, 212);
+            this._lblEarned.TabIndex = 0;
+            this._lblEarned.Text = "0.00kr";
+            // 
+            // _btnOptions
+            // 
+            this._btnOptions.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this._btnOptions.BackgroundImage = global::Earner.Properties.Resources.settings;
+            this._btnOptions.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this._btnOptions.FlatAppearance.BorderSize = 0;
+            this._btnOptions.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this._btnOptions.Font = new System.Drawing.Font("Segoe UI Light", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this._btnOptions.ForeColor = System.Drawing.Color.White;
+            this._btnOptions.Location = new System.Drawing.Point(548, 226);
+            this._btnOptions.Name = "_btnOptions";
+            this._btnOptions.Size = new System.Drawing.Size(119, 79);
+            this._btnOptions.TabIndex = 1;
+            this._btnOptions.UseVisualStyleBackColor = false;
+            this._btnOptions.Click += new System.EventHandler(this.OptionsClick);
+            // 
+            // _btnStart
+            // 
+            this._btnStart.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this._btnStart.BackgroundImage = global::Earner.Properties.Resources.pause;
+            this._btnStart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this._btnStart.FlatAppearance.BorderSize = 0;
+            this._btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this._btnStart.Font = new System.Drawing.Font("Segoe UI Light", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this._btnStart.ForeColor = System.Drawing.Color.White;
+            this._btnStart.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this._btnStart.Location = new System.Drawing.Point(678, 226);
+            this._btnStart.Name = "_btnStart";
+            this._btnStart.Size = new System.Drawing.Size(119, 79);
+            this._btnStart.TabIndex = 2;
+            this._btnStart.Tag = "Stop";
+            this._btnStart.UseVisualStyleBackColor = false;
+            this._btnStart.Click += new System.EventHandler(this.StartStopClick);
+            // 
+            // _earnerTimer
+            // 
+            this._earnerTimer.Interval = 1000;
+            this._earnerTimer.Tick += new System.EventHandler(this.Tick);
+            // 
+            // _lblWorkTime
+            // 
+            this._lblWorkTime.AutoSize = true;
+            this._lblWorkTime.BackColor = System.Drawing.Color.Transparent;
+            this._lblWorkTime.Font = new System.Drawing.Font("Segoe UI Light", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this._lblWorkTime.ForeColor = System.Drawing.Color.White;
+            this._lblWorkTime.Location = new System.Drawing.Point(12, 200);
+            this._lblWorkTime.Name = "_lblWorkTime";
+            this._lblWorkTime.Size = new System.Drawing.Size(302, 99);
+            this._lblWorkTime.TabIndex = 3;
+            this._lblWorkTime.Text = "00:00:00";
+            // 
+            // _btnHide
+            // 
+            this._btnHide.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this._btnHide.BackColor = System.Drawing.Color.Transparent;
+            this._btnHide.FlatAppearance.BorderSize = 0;
+            this._btnHide.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this._btnHide.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this._btnHide.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this._btnHide.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this._btnHide.ForeColor = System.Drawing.Color.White;
+            this._btnHide.Location = new System.Drawing.Point(695, -44);
+            this._btnHide.Name = "_btnHide";
+            this._btnHide.Size = new System.Drawing.Size(108, 98);
+            this._btnHide.TabIndex = 4;
+            this._btnHide.Text = "__";
+            this._btnHide.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this._btnHide.UseVisualStyleBackColor = false;
+            this._btnHide.Click += new System.EventHandler(this.HideClick);
+            // 
+            // _topPanel
+            // 
+            this._topPanel.Location = new System.Drawing.Point(57, 0);
+            this._topPanel.Name = "_topPanel";
+            this._topPanel.Size = new System.Drawing.Size(632, 57);
+            this._topPanel.TabIndex = 5;
+            this._topPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopPanelMouseDown);
+            // 
+            // _btnClose
+            // 
+            this._btnClose.BackColor = System.Drawing.Color.Transparent;
+            this._btnClose.FlatAppearance.BorderSize = 0;
+            this._btnClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this._btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this._btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this._btnClose.Font = new System.Drawing.Font("Segoe UI Light", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this._btnClose.ForeColor = System.Drawing.Color.White;
+            this._btnClose.Location = new System.Drawing.Point(-37, -36);
+            this._btnClose.Name = "_btnClose";
+            this._btnClose.Size = new System.Drawing.Size(108, 98);
+            this._btnClose.TabIndex = 6;
+            this._btnClose.Text = "x";
+            this._btnClose.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this._btnClose.UseVisualStyleBackColor = false;
+            this._btnClose.Click += new System.EventHandler(this.CloseClick);
+            // 
+            // EarnerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 302);
+            this.Controls.Add(this._btnClose);
+            this.Controls.Add(this._topPanel);
+            this.Controls.Add(this._btnHide);
+            this.Controls.Add(this._btnOptions);
+            this.Controls.Add(this._lblWorkTime);
+            this.Controls.Add(this._btnStart);
+            this.Controls.Add(this._lblEarned);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MdiChildrenMinimizedAnchorBottom = false;
+            this.Name = "EarnerForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Earner";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label _lblEarned;
+        private Button _btnOptions;
+        private Button _btnStart;
+        private System.Windows.Forms.Timer _earnerTimer;
+        private Label _lblWorkTime;
+        private Button _btnHide;
+        private Panel _topPanel;
+        private Button _btnClose;
+    }
+}
---
+++ EarnerForm.cs
@@ -0,0 +1,109 @@
+using Earner.Properties;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Earner
+{
+    public partial class EarnerForm : Form
+    {
+        private double Earned { get; set; } = 0;
+        private double HourRate { get; set; } = 920;
+        private double FixedDailyCost { get; set; } = 0;
+        private double MaxBillableDailyHours { get; set; } = 8;
+        private TimeSpan ElapsedTime { get; set; }
+        private string CurrencySymbols { get; set; } = "kr";
+
+        private readonly Stopwatch _stopwatch = new();
+        const int WM_NCLBUTTONDOWN = 0xA1;
+        const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        
+        [DllImportAttribute("user32.dll")]
+        static extern bool ReleaseCapture();
+
+        public EarnerForm()
+        {
+            InitializeComponent();
+            InitializeCostsAndEarnings();
+        }
+
+        private void InitializeCostsAndEarnings()
+        {
+            FixedDailyCost = 40.00d * 2.00d;    // road tolls
+            FixedDailyCost += 17.5d;            // parking
+            FixedDailyCost += 30.00d * 4.00d;   // diesel + car value reduction
+            FixedDailyCost += 1500.00d / 200d;  // insurance
+            FixedDailyCost += 106.00d;          // lunch
+            Tick(this, new EventArgs());
+            _stopwatch.Start();
+            _earnerTimer.Start();
+        }
+
+        private void StartStopClick(object sender, EventArgs e)
+        {
+            if (_btnStart.Tag.ToString() == "Start")
+            {
+                _stopwatch.Start();
+                _earnerTimer.Start();
+                _btnStart.Tag = "Stop";
+                _btnStart.BackgroundImage = Resources.pause;
+            }
+            else
+            {
+                _stopwatch.Stop();
+                _earnerTimer.Stop();
+                _btnStart.Tag = "Start";
+                _btnStart.BackgroundImage = Resources.play;
+            }
+        }
+
+        private void OptionsClick(object sender, EventArgs e)
+        {
+            // TODO; Allow edit of fixed daily costs and hourly rate
+        }
+
+        private void Tick(object sender, EventArgs e)
+        {
+            double weightedEarnings = UpdateEarnings();
+            UpdateEarningsUI(weightedEarnings);
+        }
+
+        private void UpdateEarningsUI(double weightedEarnings)
+        {
+            _lblEarned.Text = $"{weightedEarnings:0,0.00}{CurrencySymbols}";
+            _lblEarned.ForeColor = weightedEarnings < 0 ? Color.Red : Color.Green;
+            _lblWorkTime.Text = $"{ElapsedTime.TotalHours:00}:{ElapsedTime.TotalMinutes:00}:{ElapsedTime.TotalSeconds:00}";
+            _lblWorkTime.ForeColor = ElapsedTime.TotalHours <= MaxBillableDailyHours ? Color.White : Color.Red;
+        }
+
+        private double UpdateEarnings()
+        {
+            ElapsedTime = _stopwatch.Elapsed;
+            if (ElapsedTime.TotalHours <= MaxBillableDailyHours)
+                Earned += HourRate / 3600;
+            double weightedEarnings = Earned - FixedDailyCost;
+            return weightedEarnings;
+        }
+
+        private void HideClick(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void TopPanelMouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                _ = SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void CloseClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
